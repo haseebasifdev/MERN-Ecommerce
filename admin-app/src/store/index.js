94dac39cfacccbad1cc9c744d61daebefb0d5dff
haseebasifdev <haseebasif147@gmail.com>
2020-12-10 18:23:38 +0500

    add redux-devtols-extention

--- admin-app/src/store/index.js
+++ admin-app/src/store/index.js
@@ -2,9 +2,11 @@ import {applyMiddleware, createStore,compose} from 'redux';
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 import reducer  from "../reducers";
 import thunk from 'redux-thunk'
-const store=createStore(reducer
-    
-    ,applyMiddleware(thunk))
+import { composeWithDevTools } from 'redux-devtools-extension';
+const store=createStore(reducer,composeWithDevTools(
+    applyMiddleware(thunk)
+    // other store enhancers if any
+  ))
 
 export default store
 
